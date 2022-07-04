949d9f1f0d2fadde31b01923416a05a57a526843
tekaneko <tekaneko@adobe.com>
2022-07-04 15:43:49 +0900

    initial code

---
+++ README.md
@@ -0,0 +1,17 @@
+# Popup Dialog Editor for Adobe Target
+
+This tool generates the code to show pop-up dialog. Copy the generated code and paste it into Target as an experience.
+
+Dialog Type:
+
+- [Standard](https://te2cjp.github.io/popupdialogeditor/standard-banner.html)
+
+<BR/>
+
+<img src="img/how_to_use_modal_dialog_editor.gif" width="100%">
+
+<BR/>
+
+## Licensing
+
+This project is licensed under the Apache V2 License. See [LICENSE](LICENSE) for more information
---
+++ custom.js
@@ -0,0 +1,22 @@
+var positionCSS = {
+    lt: "left:30px;top:30px;",
+    ct: "left:50%;margin-left:-200px;top:30px;",
+    rt: "right:30px;top:30px;",
+    lc: "left:30px;top:50%; margin-top:-80px;",
+    cc: "left:50%;margin-left:-200px;top:50%;margin-top:-80px;",
+    rc: "right:30px;top:50%; margin-top:-80px;",
+    lb: "left:30px;bottom:30px;",
+    cb: "left:50%;margin-left:-200px;bottom:30px;",
+    rb: "right:30px;bottom:30px;",
+  };
+  var SPpositionCSS = {
+    lt: "left:30px;top:30px;",
+    ct: "left:50%;margin-left:-180px;top:30px;",
+    rt: "right:30px;top:30px;",
+    lc: "left:30px;top:50%; margin-top:-80px;",
+    cc: "left:50%;margin-left:-180px;top:50%;margin-top:-80px;",
+    rc: "right:30px;top:50%; margin-top:-80px;",
+    lb: "left:30px;bottom:70px;",
+    cb: "left:50%;margin-left:-180px;bottom:70px;",
+    rb: "right:30px;bottom:70px;",
+  };
